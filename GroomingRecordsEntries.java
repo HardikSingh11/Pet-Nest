99465fd83a2597fde1ba3ba4f8e531660cc1b4a1
Hardik Singh <108993619+HardikSingh11@users.noreply.github.com>
2023-02-15 17:45:30 +0530

    Add files via upload

---
+++ GroomingRecordsEntries.java
@@ -0,0 +1,15 @@
+public class GroomingRecordsEntries {
+    //enter grooming records
+    int groomingNumber;
+    String date;
+    int cost;
+    String reviews;
+// constructor of class is called
+    GroomingRecordsEntries(int groomingNumber,String date,int cost , String reviews){
+        this.groomingNumber = groomingNumber;
+        this.date = date;
+        this.cost = cost;
+        this.reviews = reviews;
+
+    }
+}
